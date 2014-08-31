ababb063b2ebd167a9a9f0c47f6a02e380b369c4
Natalia Rodnova <nrodnova@hotmail.com>
2014-08-31 15:30:55 -0400

    Revert "Exploratory Data Analysis: Course project 1"
    This reverts commit 7c8fb5ee4cc98eccd90784e08d5fa7f067b2f0be.

--- ExploratoryData/CourseProject1/plot1.R
+++
@@ -1,8 +0,0 @@
-selectedData <- read.table("household_power_consumption_2days.txt", header=TRUE, sep=";", colClasses=c("Date", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "character"), na.strings="?")
-selectedData$DateTime <- strptime(selectedData$DateTime, format="%Y-%m-%d %H:%M:%S")
-#head(selectedData)
-
-par(mfrow=c(1,1))
-hist(selectedData$Global_active_power, xlim=c(0,6), ylim=c(0,1200), col="red", main="Global Active Power", xlab="Gobal Active Power (kilowatts)")
-dev.copy(png, file = "plot1.png")
-dev.off()
--- ExploratoryData/CourseProject1/plot3.R
+++
@@ -1,9 +0,0 @@
-selectedData <- read.table("household_power_consumption_2days.txt", header=TRUE, sep=";", colClasses=c("Date", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "character"), na.strings="?")
-selectedData$DateTime <- strptime(selectedData$DateTime, format="%Y-%m-%d %H:%M:%S")
-
-plot(x=selectedData$DateTime, y=selectedData$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
-lines(selectedData$DateTime, selectedData$Sub_metering_2, col="red")
-lines(selectedData$DateTime, selectedData$Sub_metering_3, col="blue")
-legend("topright",  y.intersp = 0.5, cex=0.5, lwd=3, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.copy(png, file = "plot3.png")
-dev.off()
